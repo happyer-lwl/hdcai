2893016472f482887972c0c4d1837cc791be1932
happyer-lwl <runlwl@163.com>
2015-06-19 00:20:07 +0800

    Initial Commit

---
+++ hdcai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hdcai
+//
+//  Created by 刘伟龙 on 15/6/19.
+//  Copyright (c) 2015年 刘伟龙. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
